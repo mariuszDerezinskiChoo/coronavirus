beee37baff4f5379eb87bcfb9ca9df356cdd9996
Mariusz Derezinski-Choo <mnd14@duke.edu>
2020-04-03 23:36:27 -0400

    finished loading from time series files

--- functions/index.js
+++ functions/index.js
@@ -4,49 +4,76 @@ const axios = require('axios')
 const options = require('./assets/github.json')
 admin.initializeApp();
 const db = admin.firestore();
-let day = 12;
+let day = 79;
+const DAY_ZERO = new Date(2020,0,22)
 
 exports.scheduledFunction = functions.pubsub.schedule('every 1 minutes').onRun((context) => {
-    console.log(options)
-    const finalAnswer = axios.get("https://api.github.com/repos/CSSEGISandData/COVID-19/contents/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv",options)
+    let confirmedURL = "https://api.github.com/repos/CSSEGISandData/COVID-19/contents/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv"
+    let deathURL = "https://api.github.com/repos/CSSEGISandData/COVID-19/contents/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv"
+    return Promise.all([updateFromCSV(confirmedURL,day,"confirmed","newConfirmed"),updateFromCSV(deathURL,day+1,"death","newDeath")]).then((status)=> {
+        day--;
+        console.log("success: " + status)
+    },error => {
+        console.log("ERROR: " + error)
+    })
+})
+function updateFromCSV(url, dayIndex, currentKey, newKey){
+    return axios.get(url,options)
         .then( (response) => {
             let promises = []
             let fileEntries = response.data.split("\n")
-            for(let i = 0; i < fileEntries.length; i++){
-                let row = fileEntries[i].split(",")
+            //console.log(fileEntries.length);
+            let header = fileEntries[0].split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/);
+            let date = getDate(header, dayIndex);
+            for(let i = 1; i < fileEntries.length; i++){
+                let row = fileEntries[i].split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/);
+                let cases = parseInt(row[dayIndex])
                 if(row[1]==="US"){
-                    let newCases = day > 12 ? parseInt(row[day]) - parseInt(row[day-1]) : 0;
-                    let newData = {
-                        "confirmed" : parseInt(row[day]),
-                        "new_confirmed" : newCases
-                    }
+                    let formattedDate = formatDate(date);
                     let state = row[6];
-                    let dateSplit = row[day].split("/")
-                    if(dateSplit[0].length == 1){
-                        dateSplit[0] = "0" + dateSplit[0]
-                    }
-                    dateSplit[2] = "20" + dateSplit[2]
-                    let date = dateSplit.join("-")
                     let county = row[5]
+                    let newCases = date.getTime() > DAY_ZERO.getTime() ? parseInt(row[dayIndex]) - parseInt(row[dayIndex-1]) : cases;
+                    let newData = {
+                        "inferred" : false,
+                        "county" : county,
+                        "date" : formattedDate
+                    }
+                    
+                    newData[currentKey] = cases;
+                    newData[newKey] = newCases;
+
+                    
+                    //console.log(county + row[dayIndex-1])
                     promises.push(db.collection("Countries")
                         .doc("United States")
                         .collection(state)
-                        .doc(date + "_" + county)
+                        .doc(formatDate(date) + "_" + county)
                         .set(newData,{ merge: true }))
                 }
             }
             return Promise.all(promises)
-                .then((status)=> {
-                    day++;
-                    console.log(status)
-                },error => {
-                    console.log("ERROR"+error)
-                })
         }, error => {
             console.log(error)
             return Promise.resolve(1);
         })
-    console.log(finalAnswer)
-    return finalAnswer;
-})
-    
+}
+
+function formatDate(date) {
+    let d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) 
+        month = '0' + month;
+    if (day.length < 2) 
+        day = '0' + day;
+
+    return [year, month, day].join('-');
+}
+
+function getDate(header, index){
+    let dateSplit = header[index].split("/")
+    console.log(dateSplit);
+    return new Date(2000+parseInt(dateSplit[2]),parseInt(dateSplit[0])-1,parseInt(dateSplit[1]));
+}
