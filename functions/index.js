7e4dbcdf8ffba404b57ffe89d27e90d8c20aa9c0
Mariusz Derezinski-Choo <mnd14@duke.edu>
2020-04-12 22:58:16 -0400

    working on johns hopikins data

--- functions/index.js
+++ functions/index.js
@@ -1,22 +1,73 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin')
+// Fetch the service account key JSON file contents
+var serviceAccount = require("./firestore_admin.json");
+
+// Initialize the app with a service account, granting admin privileges
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://coronavirus-121.firebaseio.com"
+});
 const axios = require('axios')
 const options = require('./assets/github.json')
-admin.initializeApp();
 const db = admin.firestore();
 let day = 79;
 const DAY_ZERO = new Date(2020,0,22)
 
-exports.scheduledFunction = functions.pubsub.schedule('every 1 minutes').onRun((context) => {
+
+exports.backfill = functions.pubsub.schedule('every 5000 minutes').onRun((context) => {
     let confirmedURL = "https://api.github.com/repos/CSSEGISandData/COVID-19/contents/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv"
     let deathURL = "https://api.github.com/repos/CSSEGISandData/COVID-19/contents/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv"
     return Promise.all([updateFromCSV(confirmedURL,day,"confirmed","newConfirmed"),updateFromCSV(deathURL,day+1,"death","newDeath")]).then((status)=> {
         day--;
         console.log("success: " + status)
-    },error => {
-        console.log("ERROR: " + error)
     })
 })
+
+
+exports.checkForCommits = functions.pubsub.schedule('every 1 minutes').onRun((context) => {
+    return axios.get("https://api.github.com/repos/CSSEGISandData/COVID-19/contents/csse_covid_19_data/csse_covid_19_daily_reports",options)
+        .then((result) => {
+            let fileList = result.data;
+            let promises = [];
+            for(let i = 0; i < fileList.length; i++){
+                if(fileList[i].name.indexOf(".csv") > -1){
+                    let dateRaw = fileList[i].name.split(".")[0].split("-") //not ideal, fix
+                    let dateISO = formatDate(new Date(parseInt(dateRaw[2]),parseInt(dateRaw[0])-1,parseInt(dateRaw[1])))
+                    let commitID = fileList[i].sha
+                    promises.push(db.collection("validation").doc("UnitedStates").collection("commits").doc(dateISO).get()
+                        .then( (doc) => {
+                            if(!doc.exists || doc.data().commit != commitID){
+                                let fileContents = {
+                                    "commit" : commitID,
+                                    "date" : dateISO,
+                                    "updated" : false,
+                                }
+                                return db.collection("validation")
+                                        .doc("UnitedStates")
+                                        .collection("commits")
+                                        .doc(dateISO)
+                                        .set(fileContents)
+                            } else {
+                                return Promise.resolve(1);
+                            }
+                        }))
+                }
+            }
+            return Promise.all(promises);
+        })
+})
+
+exports.fetchNewDay = functions.firestore.document('validation/UnitedStates/commits/{day}').onUpdate((change, context) => {
+    console.log("changed! %j %s" , change.after.data() , context.params.day)
+    return null;
+});
+// New day document, write NEW
+function propagateInferredDates(){
+    return null;
+}
+// Updated day document, CORRECT
+
 function updateFromCSV(url, dayIndex, currentKey, newKey){
     return axios.get(url,options)
         .then( (response) => {
@@ -45,7 +96,7 @@ function updateFromCSV(url, dayIndex, currentKey, newKey){
                     
                     //console.log(county + row[dayIndex-1])
                     promises.push(db.collection("Countries")
-                        .doc("United States")
+                        .doc("UnitedStates")
                         .collection(state)
                         .doc(formatDate(date) + "_" + county)
                         .set(newData,{ merge: true }))
--- functions/index12.js
+++ functions/index12.js
@@ -63,3 +63,92 @@ exports.scheduledFunction = functions.pubsub.schedule('every 5 minutes').onRun((
         })
     return finalAnswer;
 })
+const functions = require('firebase-functions');
+const admin = require('firebase-admin')
+const axios = require('axios')
+const options = require('./assets/github.json')
+admin.initializeApp();
+const db = admin.firestore();
+let iteration = 65;
+
+//how to fix mistakes
+exports.scheduledFunction = functions.pubsub.schedule('every 100 minutes').onRun((context) => {
+    let states = ['Alabama','Alaska','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','District of Columbia','Florida','Georgia','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Ohio','Oklahoma','Oregon','Pennsylvania','Puerto Rico','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virginia','Washington','West Virginia','Wisconsin','Wyoming']
+    let promises = []
+    for(let i = 0; i < states.length; i++){
+        promises.push(db.collection("Countries").doc("United States").collection(states[i])
+        .where("date","==","NaN-NaN-NaN").get()
+        .then((snap) => {
+            let snapPromises = []
+            snap.forEach((doc) => {
+                snapPromises.push(doc.ref.delete())
+            })
+            return Promise.all(snapPromises)
+        }))
+    }
+    return Promise.all(promises);
+})
+    
+
+
+
+
+
+
+exports.scheduledFunction = functions.pubsub.schedule('every 5 minutes').onRun((context) => {
+    //let db = admin.firestore();
+    
+    let finalAnswer = axios.get("https://api.github.com/repos/CSSEGISandData/COVID-19/contents/csse_covid_19_data/csse_covid_19_daily_reports",options)
+        .then((result) => {
+            let fileList = result.data;
+            let promises = [];
+            for(let i = 65; i < 67; i++){
+                if(fileList[i].name.indexOf(".csv") > -1){
+                    let date = fileList[i].name.split(".")[0] //not ideal, fix
+                    promises.push(axios.get(fileList[i].url,options))//.then(day =>{ return day.data.split('\n') }));
+                }
+            }
+            console.log(iteration);
+            iteration++;
+            return Promise.all(promises);
+        })
+        .then( (entries) => {
+            let dbPromises = [];
+            for(var k = 0; k < entries.length; k++){
+                let date = entries[k].config.url.split(".csv")[0].split("csse_covid_19_daily_reports/")[1];
+                let data = entries[k].data
+                let fileData = data.split("\n")
+
+                for(var j = 0; j < fileData.length; j++){
+                    let entry = fileData[j].split(",")
+                    if(entry[3] === "US"){
+                        dbPromises.push(
+                            db.collection("Countries").doc("United States").collection(entry[2]).doc(date + "_" + entry[1]).set({
+                            "date" : date,
+                            "county" : entry[1],
+                            "data" : {
+                                "cases" : entry[7],
+                                "confirmed" : entry[8],
+                                "recovered" : entry[9],
+                                "active" : entry[10]
+                            }
+                        },{ merge: true })
+                        .then( (resolved) => {
+                            set = {}
+                            set[date]="complete"
+                            //return Promise.resolve(resolved);
+                            return db.collection("success").doc(entry[1]+"_"+entry[2]+"_"+date).set({"done":"done"})
+                         })
+                        )                          
+                    }
+                }
+            }
+        console.log(dbPromises.length)
+        return Promise.all(dbPromises).then((status)=> {
+            console.log(status)
+        },error => {
+            console.log("caught the error here: "+error)
+        })
+        })
+    return finalAnswer;
+})
