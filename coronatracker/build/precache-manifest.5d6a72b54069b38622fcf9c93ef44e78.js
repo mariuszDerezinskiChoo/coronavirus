d2241a44ab222faad3ec01c9510343954695ac4e
Mariusz Derezinski-Choo <mnd14@duke.edu>
2020-04-17 21:26:04 -0400

    fixed yesterday but

---
+++ coronatracker/build/precache-manifest.5d6a72b54069b38622fcf9c93ef44e78.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbe71fdd5e1dcf54680e0044db890ee6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0856a3ebee77c2fd74c5",
+    "url": "/static/css/main.3187a6b0.chunk.css"
+  },
+  {
+    "revision": "5f47a9533c041c12c7d1",
+    "url": "/static/js/2.923b6043.chunk.js"
+  },
+  {
+    "revision": "0856a3ebee77c2fd74c5",
+    "url": "/static/js/main.59b30bec.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
